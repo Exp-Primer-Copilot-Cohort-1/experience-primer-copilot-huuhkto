8c0740848dc50d59738dcd41427c1af58d494b80
huuhkto <107926291+huuhkto@users.noreply.github.com>
2023-10-28 06:40:19 +0000

    Copilot second commit

---
+++ members.js
@@ -0,0 +1,37 @@
+function skillsMember() {
+    var skills = [
+        {
+            "id": "1",
+            "name": "HTML",
+            "level": "80%"
+        },
+        {
+            "id": "2",
+            "name": "CSS",
+            "level": "70%"
+        },
+        {
+            "id": "3",
+            "name": "Javascript",
+            "level": "60%"
+        },
+        {
+            "id": "4",
+            "name": "PHP",
+            "level": "50%"
+        },
+        {
+            "id": "5",
+            "name": "MySQL",
+            "level": "40%"
+        },
+        {
+            "id": "6",
+            "name": "Python",
+            "level": "30%"
+        }
+    ];
+    return skills;
+}
+
+  
